7df8a1582541bf74f7481890f597bf0a4f04f87a
Wali Mirza <walimirza9982@gmail.com>
2025-09-28 11:29:10 -0400

    Finished PushBlock

--- experiments/PushBlock/ppo/result.py
+++ experiments/PushBlock/ppo/result.py
@@ -25,7 +25,7 @@ def play(create_env, policy, timestamps):
     env = create_env()
 
     with torch.no_grad():
-        tensordict_data = env.rollout(timestamps, policy=policy, auto_cast_to_device=True).to(policy.device)
+        tensordict_data = env.rollout(timestamps, policy=policy, auto_cast_to_device=True, break_when_any_done=False).to(policy.device)
         env.close()
         metrics = compute_single_trajectory_metrics(tensordict_data)
     
