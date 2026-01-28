bc9f8db2c2d897291d15e190688382ad5e3c3fe6
Wali Mirza <127229451+notDroid@users.noreply.github.com>
2026-01-28 11:44:41 -0500

    Modify install.sh for grpcio and toolkit updates

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ echo "--- Creating Conda Environment 'mlagents' ---"
 conda create -n mlagents python=3.10.12 -y
 
 echo ""
-echo "--- Installing Grpcio (Conda Forge) ---"
-conda install -n mlagents -c anaconda --override-channels grpcio=1.48.2 -y
+echo "--- Installing Grpcio (Anaconda) ---"
+conda install -n mlagents -c anaconda --override-channels grpcio=1.48.2 python=3.10.12 -y
 
 echo ""
 echo "--- Installing ML-Agents (Sequential install to bypass resolver) ---"
@@ -18,7 +18,7 @@ conda run -n mlagents python -m pip install -U numpy
 
 echo ""
 echo "--- Installing Toolkit ---"
-conda run -n mlagents python -m pip install pandas matplotlib ipykernel hydra-core seaborn huggingface_hub torchinfo
+conda run -n mlagents python -m pip install pandas matplotlib ipykernel hydra-core seaborn huggingface_hub torchinfo tensorboard
 
 echo ""
 echo "--- Installing PyTorch & TorchRL ---"
